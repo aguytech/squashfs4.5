068e5f4bd8f380562041a74c65cd22829fd0c677
aguy tech <aguytech@free.fr>
2022-07-22 13:40:00 +0200

    Adds temporary folder for installation

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 ! hash apt 2>/dev/null && echo "This installation is for Ubuntu OS" && exit 1
 
+echo "> Create temporary folder"
+path=/tmp/squashfs.$[date +%s)
+mkdir ${path} || { echo "Unable to create path; ${path}" && exit 1; }
+cd ${path}
+
 echo "> Get package from sourceforge"
 echo
 file=squashfs-tools-4.5.1.tar.gz
@@ -27,4 +32,3 @@ sudo make install || exit 1
 
 echo
 echo "> Installation succeded"
-
